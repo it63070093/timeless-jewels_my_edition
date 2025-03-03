4ce2926acaf43b487b00fa5bc495a50607c7aa11
it63070093 <74042758+it63070093@users.noreply.github.com>
2025-03-04 05:21:05 +0700

    Update skill_tree.ts

--- frontend/src/lib/skill_tree.ts
+++ frontend/src/lib/skill_tree.ts
@@ -469,6 +469,6 @@ export const constructQuery = (jewel: number, conqueror: string, result: SearchW
 
 export const openTrade = (jewel: number, conqueror: string, results: SearchWithSeed[]) => {
   const url = new URL('https://www.pathofexile.com/trade/search/Necropolis');
-  url.searchParams.set('q', JSON.stringify(constructQuery(jewel, conqueror, results)));
+  url.searchParams.set('q', JSON.stringify(constructQuery(jewel, "Rakiata", results)));
   window.open(url, '_blank');
 };
